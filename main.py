f27c3ccbf973570b4d21dc708a13a2081b785071
masato-ishizuka <masatoishizuka@ucla.edu>
2023-06-14 20:13:27 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+import io
+
+web_apps = st.sidebar.selectbox("Select Web Apps",
+                                ("Exploratory Data Analysis", "Distributions"))
+
+if web_apps == "Exploratory Data Analysis":
+
+    uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        df = pd.read_csv(uploaded_file)
+        show_df = st.checkbox("Show Data Frame", key="disabled")
+
+        if show_df:
+            st.write(df)
+
+        column_type = st.sidebar.selectbox('Select Data Type',
+                                           ("Numerical", "Categorical", "Bool", "Date"))
+
+        if column_type == "Numerical":
+            numerical_column = st.sidebar.selectbox(
+                'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+
+            # Five-number summary
+            summary = df[numerical_column].describe()
+
+            if show_df:
+                st.write(summary)
+
+            # Histogram
+            choose_color = st.color_picker('Pick a Color', "#69b3a2")
+            choose_opacity = st.slider(
+                'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+
+            hist_bins = st.slider('Number of bins', min_value=5, max_value=150, value=30)
+            hist_title = st.text_input('Set Title', 'Histogram')
+            hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+
+            fig, ax = plt.subplots()
+            ax.hist(df[numerical_column], bins=hist_bins, edgecolor="black", color=choose_color, alpha=choose_opacity)
+            ax.set_title(hist_title)
+            ax.set_xlabel(hist_xtitle)
+            ax.set_ylabel('Count')
+
+            st.pyplot(fig)
+            filename = "plot.png"
+            fig.savefig(filename, dpi=300)
+
+            # Display the download button
+            with open("plot.png", "rb") as file:
+                btn = st.download_button(
+                    label="Download image",
+                    data=file,
+                    file_name="flower.png",
+                    mime="image/png"
+                )
+
+        elif column_type == "Categorical":
+            categorical_column = st.sidebar.selectbox(
+                'Select a Column', df.select_dtypes(include=['object']).columns)
+
+            # Proportions of each category level
+            proportions = df[categorical_column].value_counts(normalize=True)
+
+            if show_df:
+                st.write(proportions)
+
+            # Customized bar plot
+            fig, ax = plt.subplots()
+            category_counts = df[categorical_column].value_counts()
+            ax.bar(category_counts.index, category_counts.values)
+
+            ax.set_title(f'Bar Plot - {categorical_column}')
+            ax.set_xlabel(categorical_column)
+            ax.set_ylabel('Count')
+
+            plt.xticks(rotation=90)
+            st.pyplot(fig)
